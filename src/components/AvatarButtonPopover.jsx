0bd9f8ef711715dbf1c7044abc641d4d0ea2a641
Yoel Peter Chandra <yoelpc4@gmail.com>
2023-08-21 23:40:38 +0700

    fix: update avatar button popover contents

--- src/components/AvatarButtonPopover.jsx
+++ src/components/AvatarButtonPopover.jsx
@@ -1,20 +1,28 @@
 import { useState } from 'react'
-import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { Link as RouterLink } from 'react-router-dom';
 import Avatar from '@mui/material/Avatar'
 import Button from '@mui/material/Button'
 import Popover from '@mui/material/Popover'
 import List from '@mui/material/List';
+import Link from '@mui/material/Link';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
-import ManageAccountsOutlinedIcon from '@mui/icons-material/ManageAccountsOutlined.js';
+import PlayCircleOutlinedIcon from '@mui/icons-material/PlayCircleOutlined';
+import PortraitOutlinedIcon from '@mui/icons-material/PortraitOutlined';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
 import ListItemText from '@mui/material/ListItemText';
 import Divider from '@mui/material/Divider';
-import ListItemLogout from '@/components/ListItemLogout.jsx';
 import Box from '@mui/material/Box';
+import ListItemLogout from '@/components/ListItemLogout.jsx';
+import { selectUser } from '@/store/auth.js';
+import Typography from '@mui/material/Typography';
 
 export default function AvatarButtonPopover() {
   const [anchorEl, setAnchorEl] = useState(null)
+
+  const user = useSelector(selectUser)
 
   const isOpen = !!anchorEl
 
@@ -25,7 +33,7 @@ export default function AvatarButtonPopover() {
   return (
     <>
       <Button onClick={handleClick}>
-        <Avatar alt="avatar" src="https://i.pravatar.cc/200" />
+        <Avatar alt="avatar" src="https://i.pravatar.cc/200"/>
       </Button>
 
       <Popover
@@ -44,21 +52,68 @@ export default function AvatarButtonPopover() {
         <Box sx={{width: '100%', maxWidth: 360, bgcolor: 'background.paper'}}>
           <nav>
             <List>
-              <ListItem disablePadding>
-                <Link to="/account" style={{textDecoration: 'none', color: 'inherit'}} onClick={handleClose}>
-                  <ListItemButton>
-                    <ListItemIcon>
-                      <ManageAccountsOutlinedIcon/>
-                    </ListItemIcon>
+              <ListItem alignItems="flex-start" onClick={handleClose}>
+                <ListItemAvatar>
+                  <Avatar alt="avatar" src="https://i.pravatar.cc/200"/>
+                </ListItemAvatar>
 
-                    <ListItemText primary="Account"/>
-                  </ListItemButton>
-                </Link>
+                <ListItemText
+                  primary={user.name}
+                  secondary={
+                    <>
+                      <Typography variant="body2" sx={{color: '#000', mb: '5px'}}>
+                        @{user.username}
+                      </Typography>
+
+                      <RouterLink
+                        to="/account"
+                        style={{textDecoration: 'none'}}
+                        onClick={handleClose}
+                      >
+                        Manage your account
+                      </RouterLink>
+                    </>
+                  }
+                />
               </ListItem>
 
               <Divider/>
 
-              <ListItemLogout />
+              <li>
+                <ListItem
+                  component={RouterLink}
+                  to={`/channel/${user.username}`}
+                  disablePadding
+                  sx={{textDecoration: 'none', color: 'inherit'}}
+                >
+                  <ListItemButton onClick={handleClose}>
+                    <ListItemIcon>
+                      <PortraitOutlinedIcon/>
+                    </ListItemIcon>
+
+                    <ListItemText primary="Your Channel"/>
+                  </ListItemButton>
+                </ListItem>
+              </li>
+
+              <li>
+                <ListItem
+                  component={Link}
+                  href={import.meta.env.VITE_STUDIO_URL}
+                  disablePadding
+                  style={{textDecoration: 'none', color: 'inherit'}}
+                >
+                  <ListItemButton onClick={handleClose}>
+                    <ListItemIcon>
+                      <PlayCircleOutlinedIcon/>
+                    </ListItemIcon>
+
+                    <ListItemText primary="MyTube Studio"/>
+                  </ListItemButton>
+                </ListItem>
+              </li>
+
+              <ListItemLogout/>
             </List>
           </nav>
         </Box>
